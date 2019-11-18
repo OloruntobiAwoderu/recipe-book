dc2642962bc47bac7ede2bc2748c830a232d6270
oloruntobi <awoderuoloruntobi@gmail.com>
2019-11-18 18:28:38 +0100

    Finished user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,71 +1,88 @@
 const router = require("express").Router();
 const Users = require("../helpers/userModel");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs")
-const restricted = require('../auth/authMiddleware')
-
+const bcrypt = require("bcryptjs");
+const restricted = require("../auth/authMiddleware");
 
 router.post("/register", (req, res) => {
-    let user = req.body;
-    const hash = bcrypt.hashSync(user.password, 10);
-    user.password = hash;
-  
-    Users.insert(user)
-      .then(created => {
-        const token = generateToken(created);
-        res.status(201).json({ user: created, token });
-      })
-      .catch(err => {
-        res.status(500).json(err);
-      });
-  });
-  
-  // Log in as user
-  router.post("/login", (req, res) => {
-    const { email, password } = req.body;
-  
-    Users.findBy({ email })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          const token = generateToken(user);
-          res.status(200).json({
-            message: `Welcome, ${user.name}!`,
-            token
-          });
-        } else {
-          res.status(401).json({ message: "You shall not pass!" });
-        }
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
-  
-  // Fetch users
-  router.get("/users", restricted, (req, res) => {
-    const { email } = req.decodedToken;
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
 
-      Users.findBy({ email })
-        .then(users => {
-          res.json({ users });
+  Users.insert(user)
+    .then(created => {
+      const token = generateToken(created);
+      res.status(201).json({ user: created, token });
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+// Log in as user
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  Users.findBy({ email })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Welcome, ${user.name}!`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: "You shall not pass!" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.put("/user/:id", restricted, (req, res) => {
+  const id = req.params.id;
+  Users.update(id, req.body)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(error => {
+      res.status(500).json({ error });
+    });
+});
+
+router.delete("/user/:id", restricted, (req, res) => {
+    const id = req.params.id;
+    Users.remove(id)
+    .then(user => {
+        res.status(200).json({
+            message: `User with the ${id} has been deleted`,
+            user
         })
-        .catch(err => res.status(500).send(err));
-    
-  });
-  
-  function generateToken(user) {
-    const payload = {
-     id: user.id,
-      email: user.email,
-      name: user.name
-    };
-    const options = {
-      expiresIn: "1d"
-    };
-    // eslint-disable-next-line no-undef
-    return jwt.sign(payload, process.env.JWT_SECRET || "oloruntobi", options);
-  }
-  
-  module.exports = router;
+    })
+})
+// Fetch users
+router.get("/users", restricted, (req, res) => {
+  Users.find()
+    .then(users => {
+      res.json({ users });
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+function generateToken(user) {
+  const payload = {
+    id: user.id,
+    email: user.email,
+    name: user.name
+  };
+  const options = {
+    expiresIn: "1d"
+  };
+  // eslint-disable-next-line no-undef
+  return jwt.sign(payload, process.env.JWT_SECRET || "oloruntobi", options);
+}
+
+module.exports = router;
