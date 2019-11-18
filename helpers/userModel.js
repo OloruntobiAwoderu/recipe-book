624bd848b7fad4ee9320a39ce4698fba81485a5b
oloruntobi <awoderuoloruntobi@gmail.com>
2019-11-18 13:39:15 +0100

    Added helper function for user routes

---
+++ helpers/userModel.js
@@ -0,0 +1,43 @@
+const knex = require("knex");
+const knexConfig = require("../../knexfile");
+const db = knex(knexConfig.development);
+
+module.exports = {
+  find,
+  findById,
+  insert,
+  update,
+  remove,
+  findBy
+};
+
+function find() {
+  return db("users");
+}
+
+function findById(id) {
+  return db("users")
+    .where({ id: Number(id) })
+    .first();
+}
+
+function findBy(filter) {
+  return db("users").where(filter);
+}
+function insert(user) {
+  return db("users")
+    .insert(user)
+    .then(ids => ({ id: ids[0] }));
+}
+
+function update(id, user) {
+  return db("users")
+    .where("id", Number(id))
+    .update(user);
+}
+
+function remove(id) {
+  return db("users")
+    .where("id", Number(id))
+    .del();
+}
