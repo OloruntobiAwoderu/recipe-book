1851ebb5bf2772b96e1194bd313e410946eb0309
oloruntobi <awoderuoloruntobi@gmail.com>
2019-11-18 14:12:49 +0100

    Added production to knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,6 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
     client: "sqlite3",
     connection: {
@@ -13,12 +12,17 @@ module.exports = {
     },
     seeds: {
       directory: "./data/seeds"
+    },
+    production: {
+      client: "pg",
+      connection: process.env.DATABASE_URL,
+      useNullAsDefault: true,
+      migrations: {
+        directory: "./data/migrations"
+      },
+      seeds: {
+        directory: "./data/seeds"
+      }
     }
-  },
-
-
- 
-
-
-
+  }
 };
